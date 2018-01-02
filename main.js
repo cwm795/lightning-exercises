a67a110696c51041754eae58d11d26876dda62ad
Chandler Morrison <cwm795@gmail.com>
2018-01-02 10:11:56 -0600

    pushing up todays work

--- main.js
+++ main.js
@@ -1,26 +1,71 @@
-var array = [
-{
-    name: "Bob",
-    department: "sales",
-    title: "sales manager"
-}, {
-    name: "Tina",
-    department: "finance",
-    title: "director of finance"
-}, {
-    name: "Randy",
-    department: "IT",
-    title: "hardware guy"
-}, {
-    name: "Glenda", department: "C - suite",
-    title: "CEO"
-}
-]
+// // // // var array = [
+// // // // {
+// // // //     name: "Bob",
+// // // //     department: "sales",
+// // // //     title: "sales manager"
+// // // // }, {
+// // // //     name: "Tina",
+// // // //     department: "finance",
+// // // //     title: "director of finance"
+// // // // }, {
+// // // //     name: "Randy",
+// // // //     department: "IT",
+// // // //     title: "hardware guy"
+// // // // }, {
+// // // //     name: "Glenda", department: "C - suite",
+// // // //     title: "CEO"
+// // // // }
+// // // // ]
 
 
-for (let i = 0; i < array.length; i++){
-console.log( array[i].name + "’s job title is " + array[i].title );
-}
+// // // // for (let i = 0; i < array.length; i++){
+// // // // console.log( array[i].name + "’s job title is " + array[i].title );
+// // // // }
+
+// // // var car = {type:"Ford", model: "focus", year: "2012"};
+
+// // // var specificCar = car.type;
+// // // console.log();
+
+// // let ages = [12, 23, 89, 16, 34, 19, 2, 7, 45, 50, 26, 102]
+
+// // copy = [];
+
+// // for (let i = 0; i < ages.length; i++) {
+// // if( ages < 50 && ages > 17)
+// // }
+
+// // ages.forEach((ages) => {
+// // copy.push(ages);
+// // console.log("hey");
+// // })
+// hitchhikers_guide = {
+//   characters: ["Zaphod", "Arthur", "Ford", "Trillian"],
+//   catchphrase: "Don't Panic",
+//   random_facts: {
+//     copies_sold: 14000000,
+//     formats: ["radio", "TV", "film", "graphic novel"],
+//     ultimate_answer: {
+//       meaning_of_life: 42
+//     }
+//   }
+// };
+
+
+// console.log(hitchhikers_guide.random_facts.ultimate_answer.meaning_of_life, "hey");
+
+let yoda_quote = ["the", "greatest", "teacher,", "failure", "is"];
+
+var test = yoda_quote.join( " " );
+let test1 = test
+
+console.log(test)
+
+
+
+
+
+
 
 
 
